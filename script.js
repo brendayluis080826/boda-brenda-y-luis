8757766206eca3a358278e058792165564a6853e
brendayluis080826 <brendayluis080826@gmail.com>
2026-07-28 02:08:01 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+
+    const splash = document.getElementById("splash");
+
+    setTimeout(() => {
+
+        splash.style.opacity = "0";
+        <div class="loading-heart">
+
+♡
+
+</div>
+
+        setTimeout(() => {
+
+            splash.style.display = "none";
+
+        },800);
+
+    },2200);
+
+});
